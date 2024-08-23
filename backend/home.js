57121c2cb2876a98c5cf5eb8240c851b451dae1d
brmattosbus <brmattosbus@gmail.com>
2024-08-23 16:41:52 -0700

    minor style fix

--- backend/home.js
+++ backend/home.js
@@ -6,9 +6,31 @@ Description:
     the pages/features
 */
 
-const fretboardImg = document.querySelector(".freatboard-img");
+const fretboardImg = document.querySelector(".fretboard-img");
 const libraryImg = document.querySelector(".library-img");
 const toolsImg = document.querySelector(".tools-img");
 const fretboardLink = document.getElementById("fretboard-link");
 const libraryLink = document.getElementById("library-link");
 const toolsLink = document.getElementById("tools-link");
+
+// Page images links
+fretboardImg.addEventListener("click", function() {
+    window.location.href = "../pages/fretboard.html";
+});
+libraryImg.addEventListener("click", function() {
+    window.location.href = "../pages/library.html";
+});
+toolsImg.addEventListener("click", function() {
+    window.location.href = "../pages/tools.html";
+});
+
+// Feature title links
+fretboardLink.addEventListener("click", function() {
+    window.location.href = "../pages/fretboard.html";
+});
+libraryLink.addEventListener("click", function() {
+    window.location.href = "../pages/library.html";
+});
+toolsLink.addEventListener("click", function() {
+    window.location.href = "../pages/tools.html";
+});
